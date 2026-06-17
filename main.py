19906186e3548b585954ea9f198ccf600fee3dfd
gilberto <gilbertogulart70@gmail.com>
2026-06-17 20:32:51 -0300

    Projeto finalizado

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import webbrowser
 from cadastro import cadastro
 from relatorioCadastro import listaProdutos, salvaProdutos
-from relatorios import relatorioEstoque
+from relatorioEstoque import relatorioEstoque
 
 while True:
     def menu():
@@ -10,27 +10,56 @@ while True:
         print(f"1 - Cadastrar Produtos")
         print(f"2 - Listar produtos")
         print(f"3 - Gerar Relatório de Estoque")
-        print(f"4 - Sair")
+        print(f"4 - Remover SKU")
+        print(f"5 - Sair")
     
     menu()
     option = int(input(">>> "))
     os.system('cls')
     
-    if option not in [1,2,3,4]:
+    if option not in [1,2,3,4,5]:
         menu()
         option = int(input(">>> "))
 
     if option == 1:
-        produtos = cadastro()
-        salvaProdutos(produtos)
+        
+        try:
+            produtos = listaProdutos()
+            novo_produto = cadastro()
+            produtos.extend(novo_produto)  
+            
+            salvaProdutos(produtos)
+            
+        except:
+            produtos = cadastro()
+            salvaProdutos(produtos)
+        
         print('Produto Cadastrado!')
+        
     if option == 2:
         listaProdutos()
         webbrowser.open('produtos.txt')
+        
     if option == 3:
         produtos = listaProdutos()
         relatorioEstoque(produtos)
         webbrowser.open('estoque.txt')
-        
+    
     if option == 4:
+        produtos = listaProdutos()
+        sku = str(input('Digite o sku que deseja remover: '))
+        
+        for produto in produtos:
+            if produto[0] == sku:
+                produtos.remove(produto)
+                salvaProdutos(produtos)
+                print("Produto removido!")
+                webbrowser.open('produtos.txt')
+                    
+                    
+            else:
+                sku = str(input('Digite um sku válido: '))
+        
+    if option == 5:
         break
+    
--- relatorioCadastro.py
+++ relatorioCadastro.py
@@ -5,21 +5,10 @@ from cadastro import cadastro
 def salvaProdutos(produtos):
     data_atual = datetime.now()
 
-    try:
-        with open('produtos.txt', 'r', encoding='utf8') as arquivo:
-            linhas = arquivo.readlines()
-            
-            produtos_antigos = linhas[4:]
-            
-    except FileNotFoundError:
-        produtos_antigos = []
-        
-
     with open('produtos.txt', 'w', encoding='utf8') as arquivo:
         arquivo.write(f"Relatório de Produtos Cadastrados - {data_atual.strftime('%d/%m/%Y %H:%M:%S')}\n\n")
         arquivo.write(f"SKU ------------------ Produto ------------------ UN ------------------ Custo\n\n")
     
-        arquivo.writelines(produtos_antigos)
     
     with open('produtos.txt', 'a', encoding='utf8') as arquivo:
         for produto in produtos:
@@ -40,7 +29,9 @@ def listaProdutos():
         for linha in arquivo:
             data = linha.strip().split(' ------------------ ')
             produtos.append(
-                [data[0], data[1], data[2] , float(data[3].replace("R$", ""))]
+                [str(data[0]), data[1], data[2] , float(data[3].replace("R$", ""))]
             )
             
     return produtos
+
+        
